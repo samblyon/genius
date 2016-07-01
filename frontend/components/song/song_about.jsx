c0bfce53bfe6bb626e3b977a85b67c7050091c66
Samuel Lyon <sblyon@me.com>
2016-06-30 18:35:11 -0700

    completed basic song page styling

--- frontend/components/song/song_about.jsx
+++ frontend/components/song/song_about.jsx
@@ -3,9 +3,13 @@ const React = require('react');
 const SongAbout = React.createClass({
 
   render() {
+    let about =  "No about content for this song...";
+    if (this.props.song.about) {
+      about = this.props.song.about;
+    }
     return (
-      <div class="song-about">
-        {this.props.song.about}
+      <div className="song-about">
+        {about}
       </div>
     );
   }
