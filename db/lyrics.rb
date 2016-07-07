b9d40f735277346390d5cac40171c143e1c20b28
Samuel Lyon <sblyon@me.com>
2016-07-07 13:43:32 -0700

    added seed songs

--- db/lyrics.rb
+++ db/lyrics.rb
@@ -1,77 +1,592 @@
-LYRICS = [
-  "
-    It took 5 women 2 getcha off of my mind
-    It took 5 months, a plenty good wasted time
-    But it just took 5 minutes when I saw your face again
-    2 fall in love all over, I guess with U I'll never win
+LYRICS = {
+  ill_make_a_man_out_of_you:
+  """Let's get down to business, to defeat the Huns
+Did they send me daughters, when I asked for sons?
+You're the saddest bunch I ever met
+But you can bet, before we're through
+Mister, I'll make a man out of you
 
-    March was a cold one in more ways than one
-    Went down 2 the nightclub lookin' 4 the sun
-    Lookin' 4 a light that could brighten up a day
-    That's been darker than a hole since U went away
+Tranquil as a forest, but on fire within
+Once you find your center, you are sure to win
+You're a spineless, pale pathetic lot
+And you haven't got a clue
+Somehow I'll make a man out of you
 
-    April usually brings showers
-    This time it was a hurtin' kind
-    This woman said she wanted my baby
-    I told her I'd have 2 be deaf, dumb, and blind
-    2 fall in love with someone that I barely knew
-    The baby would never really have a father
-    Cuz I still, cuz I still, cuz I still, still, still, huh
-    Be stuck on U
+I'm never gonna catch my breath
+Say good-bye to those who knew me
+Boy, was I a fool in school for cutting gym
+This guy's got 'em scared to death
+Hope he doesn't see right through me
+Now I really wish that I knew how to swim
 
-    It took 5 women 2 getcha off of my mind
-    Get off my mind, baby
-    Fallin' in love all over... U I'll never win
+[Chorus]
+Be a man
+We must be swift as a coursing river
+Be a man
+With all the force of a great typhoon
+Be a man
+With all the strength of a raging fire
+Mysterious as the dark side of the moon
 
-    May was her name, she was jumpin'
-    Jumpin' from plane 2 plane
-    Actress or model or somethin'
-    Can't even remember her first name
+Time is racing toward us, till the Huns arrive
+Heed my every order, and you might survive
+You're unsuited for the rage of war
+So pack up, go home, you're through
+How could I make a man out of you?
 
-    June brought an Easter bunny
-    Needless 2 say, hmph, that girl was late
-    Told a made-up story 2 a busy-body of our only date
-    What a sketch!
+[Chorus x2]""",
 
-    July was a heartbreak, extraordinary
-    Said I met this woman whose kiss was somethin' scary
-    It felt so much, so much like yours
-    But it only made me, made me want U more
-    She's gone, she's gone but U're still here
-    I want U very..., huh, it's U I fear
+  gold:
+  """Only Sunshine
+Under The Bright Sky
 
-    But it took 5 women 2 get U off my mind
-    It took 5 months, 5 months, a plenty good wasted time
-    But it just took 5 minutes and I saw your face again
-    I fell in love all over with U, I guess I'll never win
-  ",
+Only Sunshine
+Under The Bright Sky
 
-  """
-    35 MPH Town
+When The Words Are Like Bullets
+And They Break Through Your Skin
+And There's No Way To Get Them All Out
+When It Feels Like Rust Seeping Into Your Soul
 
-    Mama locked the door last night for the first time in all of her years
-    I said mama you gotta be kidding me, she said things ain't like they used to be round here
-    You just wouldn't believe it, kids growing up without Jesus, good thing your daddy ain't here to see this now
-    They stay higher than the water tower in this 35 MPH Town
+Stay Gold, Gold, Gold [X3]
 
-    She said it ain't the drugs, it ain't the booze, I've had a backroad buzz or two, myself
-    It seems like heaven is slipping further away, the devil ain't got far to go when they raise hell
-    We used to burn a hole through midnight, smoking a stolen Marlboro Light and
-    on a good night might kill a 5th of Crown But now there's a lower higher power in this 35 MPH town
+Under The Bright Sky
+Only Sunshine
 
-    Oh we can't blame the babies for growing up lazy
-    And crazy it ain't them that let them down
-    If they ain't stealing, they're suing, why work when we'll give it to 'em
-    It's right there in the bible that we don't put out
-    Spare the rod and you'll sour a 35 MPH town
+Under The Bright Sky
+Only Sunshine
 
-    The \"yes mam\"\'s gone and the \"please\"\'s too
-    It's out the door with the \"thank you\" in the evening
-    You don't see a front yard football game
-    The neighbor kids have done away with the shirt and skins
-    No one hits the front porch lights to get the kids to come inside
-    Cause the streets ain't safe for a bike to ride down
-    Since they planted a prowler in this 35 MPH town
-    No they're going nowhere fast tonight no matter how fast they drive this 35 Hour Town
-  """
-]
+Stay Gold, Gold, Gold [X6]
+
+You Were Always Searching
+For Something Out There
+Out Of Reach
+Couldn't Stop You Couldn't Hold You Back
+You Were So Real
+
+When The Words Are Like Bullets
+And They Break Through Your Skin
+And There's No Way To Get Them All Out
+When It Feels Like Rust Seeping Into Your Soul
+
+Stay Gold, Gold, Gold [X3]
+
+Under The Bright Sky
+Only Sunshine
+
+Under The Bright Sky
+Only Sunshine
+
+Stay Gold, Gold, Gold""",
+
+  aint_misbehavin:
+  """No one to talk with
+All by myself
+No one to walk with
+But I'm happy on the shelf
+Ain't misbehavin'
+I'm savin' my love for you
+
+I know for certain
+The one I love
+I'm through with flirtin'
+It's just you I'm thinkin' of
+Ain't misbehavin'
+I'm savin' my love for you
+Like Jack Horner
+In the corner
+Don't go nowhere
+What do I care?
+Your kisses are worth waitin' for
+Believe me
+
+I don't stay out late
+Don't care to go
+I'm home about eight
+Just me and my radio
+Ain't misbehavin'
+I'm savin' my love for you""",
+
+  you_and_me:
+  """[Verse 1]
+What day is it? And in what month?
+This clock never seemed so alive
+I can't keep up and I can't back down
+I've been losing so much time
+
+[Hook]
+Cause it's you and me and all of the people with nothing to do
+Nothing to lose
+And it's you and me and all other people
+And I don't know why, I can't keep my eyes off of you
+[Verse 2]
+One of the things that I want to say just aren't coming out right
+I'm tripping on words
+You've got my head spinning
+I don't know where to go from here
+
+[Hook]
+Cause it's you and me and all of the people with nothing to do
+Nothing to prove
+And it's you and me and all other people
+And I don't know why, I can't keep my eyes off of you
+
+[Bridge]
+There's something about you now
+I can't quite figure out
+Everything she does is beautiful
+Everything she does is right
+
+[Hook]
+Cause it's you and me and all of the people with nothing to do
+Nothing to lose
+And it's you and me and all other people
+And I don't know why, I can't keep my eyes off of you
+And me and all other people with nothing to do
+Nothing to prove
+And it's you and me and all other people
+And I don't know why, I can't keep my eyes off of you
+
+[Verse 1(Reused as Outro)]
+What day is it?
+And in what month?
+This clock never seemed so alive""",
+
+  hells_bells:
+  """[Bells/Guitar Intro]
+
+[Verse 1]
+I'm rolling thunder pouring rain
+I'm coming on like a hurricane
+White lightning flashing across the sky
+You're only young but you're going to die
+I won't take no prisoners won't spare no lives
+Nobody's putting up a fight
+I got my bell I'm going to take you to hell
+I'm going to get you satan get you
+[Chorus]
+Hells bells, hells bells
+Hells bells, you got me ringing
+Hells bells, my temperature's high
+Hells bells
+
+[Verse 2]
+I'll give you black sensations up and down your spine
+If you're into evil, you're a friend of mine
+See the white light flashing as I split the night
+Because if good's on the left then I'm sticking to the right
+I won't take no prisoners, won't spare no lives
+Nobody's putting up a fight
+I got my bell I'm going to take you to hell
+I'm going to get you satan get you
+
+[Chorus]
+Hells bells, hells bells
+Hells bells, you got me ringing
+Hells bells, my temperature's high
+Hells bells
+
+[Guitar Solo]
+
+Hells bells, satan coming to you
+Hells bells, he's ringing them now
+
+[Outro]
+Those hells bells, my temperature's high
+Hells bells, across the sky
+Hells bells, they're taking you down
+Hells bells, they're dragging you down
+Hells bells, going to split the night
+Hells bells, there's no way to fight""",
+
+  yellow_submarine:
+  """[Verse 1]
+In the town where I was born
+Lived a man who sailed to sea
+And he told us of his life
+In the land of submarines
+
+[Verse 2]
+So we sailed up to the sun
+Till we found the sea of green
+And we lived beneath the waves
+In our yellow submarine
+[Chorus]
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+
+[Verse 3]
+And our friends are all on board
+Many more of them live next door
+And the band begins to play
+
+[Chorus]
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+
+[Verse 4]
+Full speed ahead, Mr. Parker, full speed ahead
+Full speed over here, sir
+Action station, action station
+Aye, aye, sir, fire
+Heaven, heaven
+
+[Verse 5]
+As we live a life of ease
+Everyone of us has all we need (has all we need)
+Sky of blue (sky of blue) and sea of green (and sea of green)
+In our (In our) yellow submarine (yellow submarine, ha-ha!)
+
+[Chorus]
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine""",
+
+  a_d_g_l:
+  """太陽會出現
+只要給一次機會
+心情一飛衝天
+帶你的煩惱哎喂
+每一次
+在谷底跌倒
+來一點高潮
+Baby想給你愛的鼓勵
+在你需要的時候
+加油打氣
+Baby想給你愛的鼓勵
+雨後還能有彩虹
+別放棄
+
+太陽出現了
+感謝你給我機會
+心情哼出來是一首歌
+讓你聽的好甜美
+每一次
+在谷底跌倒
+來一點高潮
+Baby想給你愛的鼓勵
+在你需要的時候
+加油打氣
+更多更詳盡歌詞 在魔鏡歌詞網
+Baby想給你愛的鼓勵
+雨後還能有彩虹
+別放棄
+別放棄
+
+別放棄
+
+每一次
+在谷底跌倒
+來一點高潮
+Baby想給你愛的鼓勵
+在你需要的時候
+加油打氣
+Baby想給你愛的鼓勵
+雨後還能有彩虹
+別放棄
+Baby想給你愛的鼓勵
+在你需要的時候
+加油打氣
+Baby想給你愛的鼓勵
+雨後還能有彩虹
+別放棄""",
+
+  peanut_butter_jelly:
+  """[Verse]
+Sleepless nights at the château
+Visualize it
+I'll give you something to do
+
+Kush kush (cwtch cwtch?) wherever we go
+Visualize it
+I'll give you something to do
+To get a wildfire burning
+Visualize it
+I'll give you something to do
+
+Ace high, I'm going all in
+Visualize it
+I'll give you something to do
+
+[Chorus]
+Spread it like peanut butter jelly
+Do it like I owe you some money
+Do it like I owe you some money
+Money, money, money…
+
+Spread it like
+Spread it like
+Visualize it
+I'll give you something to do
+Spread it like
+
+[Verse + Chorus]
+Sleepless nights at the château
+Visualize it
+I'll give you something to do
+
+Kush kush (cwtch cwtch?) wherever we go
+Visualize it
+I'll give you something to do
+
+To get a wildfire burning
+Visualize it
+I'll give you something to do
+
+Ace high, I'm going all in
+Visualize it
+I'll give you something to do
+
+Spread it like peanut butter jelly
+Do it like I owe you some money
+Do it like I owe you some money
+Money, money, money…
+
+Spread it like
+Visualize it
+I'll give you something to do
+
+Spread it like
+Visualize it
+I'll give you something to do
+I'll give you something to do""",
+
+  destiny:
+    """I lie awake
+I've gone to ground
+I'm watching porn
+In my hotel dressing gown
+And now I dream of you
+But I still believe
+There's only enough for one
+In this lonely hotel suite
+
+[Verse 2: Sia]
+The journey's long
+And I feel so bad
+I'm thinking back to the last day we had
+[Pre-Chorus: Sia]
+Old moon fades into the new
+Soon I know I'll be back with you
+I'm nearly with you
+I'm nearly with you
+
+[Chorus: Sia & Sophie Barker]
+When I'm weak I draw strength from you
+And when you're lost I know how to change your mood
+And when I'm down you breathe life over me
+Even though we're miles apart we are each other's destiny
+
+[Verse 3: Sia]
+On a clear day
+I'll fly home to you
+I'm bending time
+Getting back to you
+
+[Pre-Chorus: Sia]
+Old moon fades into the new
+Soon I know I'll be back with you
+I'm nearly with you
+I'm nearly with you
+
+[Chorus: Sia & Sophie Barker]
+When I'm weak I draw strength from you
+And when you're lost I know how to change your mood
+And when I'm down you breathe life over me
+Even though we're miles apart we are each other's destiny
+When I'm weak I draw strength from you
+And when you're lost I know how to change your mood
+And when I'm down you breathe life over me
+Even though we're miles apart we are each other's destiny
+
+[Outro: Sia]
+I'll fly, I'll fly home
+I'll fly home and I'll fly home""",
+
+  escape_pina_colada:
+    """I was tired of my lady, we\'d been together too long
+    Like a worn out recording of a favorite song
+    So while she lay there sleepin\' I read the paper in bed
+    And in the personal columns, there was this letter I read
+
+    If you like piña coladas and getting caught in the rain
+    If you\'re not into yoga, if you have half a brain
+    If you like making love at midnight in the dunes of the cape
+    Then I\'m the love that you\'ve looked for, write to me and escape
+
+    I didn\'t think about my lady, I know that sounds kind of mean
+    But me and my old lady had fallen into the same old dull routine
+    So I wrote to the paper, took out a personal ad
+    And though I\'m nobody\'s poet, I thought it wasn\'t half bad
+
+    Yes, I like piña coladas and getting caught in the rain
+    I\'m not much into health food, I am into champagne
+    I\'ve got to meet you by tomorrow noon and cut through all this red tape
+    At a bar called O\'Malley\'s where we\'ll plan our escape
+
+    So I waited with high hopes and she walked in the place
+    I knew her smile in an instant, I knew the curve of her face
+    It was my own lovely lady and she said, \"oh, it\'s you.\"
+    Then we laughed for a moment and I said, \"I never knew.\"
+
+    That you like piña coladas and gettin\' caught in the rain
+    And the feel of the ocean and the taste of champagne
+    If you like making love at midnight in the dunes on the cape
+    You\'re the lady I\'ve looked for, come with me and escape
+
+    If you like piña coladas and getting caught in the rain
+    If you\'re not into yoga, if you have half a brain
+    If you like making love at midnight in the dunes on the cape
+    Then I\'m the love that you\'ve looked for, write to me and escape
+
+    Yes I like piña coladas and getting caught in the rain
+    I\'m not much into health food, I am into champagne
+    I\'ve got to meet you by tomorrow noon and cut through all this red tape
+    At a bar called O\'Malley\'s where we'll plan our escape""",
+
+  panda:
+  """[Intro]
+This what they all been waitin' for
+I guess so
+They been waitin' for this shit for a long time didn’t they
+I'ma give it everythin' I got
+Ayo Dougie park that X6 around the corner
+Ayy I'm just feelin' my vibe right now
+I'm feelin' myself
+
+[Bridge]
+Panda, Panda
+Panda, Panda, Panda, Panda, Panda
+
+[Hook]
+I got broads in Atlanta
+Twistin' dope, lean, and the Fanta
+Credit cards and the scammers
+Hittin' off licks in the bando
+Black X6, Phantom
+White X6 look like a panda
+Goin' out like I'm Montana
+Hundred killas, hundred hammers
+Black X6, Phantom
+White X6, panda
+Pockets swole, Danny
+Sellin' bar, candy
+Man I'm the macho like Randy
+The choppa go Oscar for Grammy
+Bitch nigga pull up ya panty
+Hope you killas understand me
+
+[Hook]
+I got broads in Atlanta
+Twistin' dope, lean, and the Fanta
+Credit cards and the scammers
+Hittin' off licks in the bando
+Black X6, Phantom
+White X6 look like a panda
+Goin' out like I'm Montana
+Hundred killas, hundred hammers
+Black X6, Phantom
+White X6, panda
+Pockets swole, Danny
+Sellin' bar, candy
+Man I'm the macho like Randy
+The choppa go Oscar for Grammy
+Bitch nigga pull up ya panty
+Hope you killas understand me
+
+[Bridge]
+Hey
+Panda, Panda
+Panda, Panda, Panda, Panda, Panda, Panda, Panda, Panda
+
+[Verse]
+I got broads in Atlanta
+Twistin' dope, lean, and shit, sippin' Fanta
+Credit cards and the scammers
+Wake up Versace shit, life Desiigner
+Whole bunch of lavish shit
+They be askin' 'round town who be clappin' shit
+I be pullin' up stuff in the Phantom ship
+I got plenty of stuff of Bugatti whip look how I drive this shit
+Black X6, Phantom
+White X6, killin' on camera
+Pop a Perc, I can't stand up
+Gorilla, they come and kill you with bananas
+Four fillas, they finna pull up in the Phantom
+Know niggas, they come and kill you on the camera
+Big Rollie, it dancin' bigger than a Pandie
+Go Oscar for Grammy, bitch pull up ya panty
+Fill up I'ma flip it, I got bitches pull up and they get it
+I got niggas that's countin' for digits
+Say you make you a lot of new money
+Know some killas pull off and they in the Wraith
+CTD, pull up in the killa Bape
+Call up Phillip-Phillip, gon' fill the bank
+Niggas up in the bank, we gon' drill the bank
+Fuck we gon' kill the bank, get it
+I got broads, yea I get it
+I get cards yea I shitted
+This how I live it
+Did it all for a ticket
+Now Flex drop bombs when he spin it
+And Bobby gon' trend it
+Jeff The Don doin' business
+Zana Ray fuckin' up shit and she doin' her business
+I be gettin' to the chicken
+Countin' to the chicken
+And all of my niggas gon' split it
+
+[Bridge]
+Panda, Panda
+Panda, Panda, Panda, Panda, Panda
+
+[Hook]
+I got broads in Atlanta
+Twistin' dope, lean, and the Fanta
+Credit cards and the scammers
+Hittin' off licks in the bando
+Black X6, Phantom
+White X6 look like a panda
+Goin' out like I'm Montana
+Hundred killas, hundred hammers
+Black X6, Phantom
+White X6, panda
+Pockets swole, Danny
+Sellin' bar, candy
+Man I'm the macho like Randy
+The choppa go Oscar for Grammy
+Bitch nigga pull up ya panty
+Hope you killas understand me
+
+[Hook]
+I got broads in Atlanta
+Twistin' dope, lean, and the Fanta
+Credit cards and the scammers
+Hittin' off licks in the bando
+Black X6, Phantom
+White X6 look like a panda
+Goin' out like I’m Montana
+Hundred killas, hundred hammers
+Black X6, Phantom
+White X6, panda
+Pockets swole, Danny
+Sellin' bar, candy
+Man I'm the macho like Randy
+The choppa go Oscar for Grammy
+Bitch nigga pull up ya panty
+Hope you killas understand me
+
+[Outro]
+Panda, Panda
+Panda, Panda, Panda, Panda, Panda"""
+
+}
