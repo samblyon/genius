5d4886cc539eb2c67349210db33af0682570c9be
Samuel Lyon <sblyon@me.com>
2016-07-10 18:44:56 -0700

    improved splash page

--- frontend/components/song/song_display.jsx
+++ frontend/components/song/song_display.jsx
@@ -33,27 +33,32 @@ const SongDisplay = React.createClass({
 
     return (
       <div>
-        <div className="song-display-splash" />
+        <div className="song-display-splash">
+          <div className="song-display-splash-text">
+            The premier location for lyrics annotation & discussion
+            inspired by rapGenius
+          </div>
+        </div>
         <div className="song-display-page">
           <div className="song-displays">
-            <div className="song-display">
-              <div className="about">
-                <p>
-                  <span className="bright">So-Genius</span> is a <span className="bright">lyrics annotation and discussion website </span>
-                  based on <a href="http://rap.genius.com">Rap Genius</a>.
-                  Rap Genius's signature highlight-to-annotate and login-in-place
-                  are constructed here using React, and draw from a Ruby on Rails
-                  API. So-Genius also features a snappy front-end authentication,
-                  eager-loading, and as-you-type search. Click a song below to jump in,
-                  or look into the developer, <a href="http://github.com/samblyon">Samuel Lyon</a>.
-                </p>
-              </div>
-            </div>
             <h3>Hot on So-Genius</h3>
             <div className="song-display">
               {tiles}
             </div>
           </div>
+          <div className="song-display">
+            <div className="about">
+              <p>
+                <span className="bright">So-Genius</span> is a <span className="bright">lyrics annotation and discussion website </span>
+                admiringly based on <a href="http://rap.genius.com">Rap Genius</a>.
+                Pick a song and jump in: beyond signature Genius features like highlight-to-annotate
+                and login-in-place, So-Genius delivers snappy front-end authentication,
+                eager-loading, and as-you-type search using React and Rails. Check out the developer,
+                Samuel Lyon, on <a href="http://github.com/samblyon">github</a> or
+                <a href="https://www.linkedin.com/in/samuellyon"> LinkedIn</a>.
+                </p>
+              </div>
+            </div>
         </div>
       </div>
     );
