36640965ca854b5bc20a3b43046db0da9e173c64
Samuel Lyon <sblyon@me.com>
2016-07-10 21:49:11 -0700

    changed to serve static files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,8 @@ Rails.application.configure do
   config.cache_classes = true
 
   # serve static assets
-  config.serve_static_assets = true
-  
+  config.serve_static_files = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
