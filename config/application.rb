a91d351e807a446c90f66e700a8832f91452c4c9
Samuel Lyon <sblyon@me.com>
2016-07-10 21:47:49 -0700

    removed precompile

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,6 @@ module Genius
 
     #serve fonts!
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     # Devise accept JSON
     config.to_prepare do
