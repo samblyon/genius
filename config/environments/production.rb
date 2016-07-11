6cf644ee3d9a66e4a4324e669ddf59a8c5c1149f
Samuel Lyon <sblyon@me.com>
2016-07-10 21:51:15 -0700

    removed static assets configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # serve static assets
-  config.serve_static_files = true
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
